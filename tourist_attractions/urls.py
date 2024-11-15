6cea90e72d814ba4b0b42ec0a21f9764ef4f2d43
M KIR AHMET <mkirahmet@gmail.com>
2024-11-15 15:59:19 +0200

    Finished project

--- tourist_attractions/urls.py
+++ tourist_attractions/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('details/<statename>', views.details, name='details')
+    path('details/<statename>', views.details, name='details'),
+    path('state/create', views.StateCreate.as_view(), name = 'statecreate'),
+    path('attraction/create', views.AttractionCreate.as_view(), name = 'attractioncreate')
 ]
--- tourist_attractions/views.py
+++ tourist_attractions/views.py
@@ -1,21 +1,24 @@
-from django.shortcuts import render
-
-# This is the dictionary for all the attractions
-attractions = [
-  { 'attraction_name' : 'Niagra Falls', 'state' : 'New York'},
-  { 'attraction_name' : 'Grand Canyon National Park', 'state' : 'Arizona'},
-  { 'attraction_name' : 'Mall of America', 'state' : 'Minnesota'},
-  { 'attraction_name' : 'Mount Rushmore', 'state' : 'South Dakota'},
-  { 'attraction_name' : 'Times Square', 'state' : 'New York'},
-  { 'attraction_name' : 'Walt Disney World', 'state' : 'Florida'}
-]
+from django.shortcuts import render, get_object_or_404
+from .models import State, Attraction
+from .forms import StateCreateForm, AttractionCreateForm
+from django.views.generic.edit import CreateView
 
 def home(request):
-  # The context is all of the variables we want passed into the template.
-  context = {"attractions" : attractions}
+  all_attractions = Attraction.objects.all()
+  context = {"attractions" : all_attractions}
   return render(request, 'tourist_attractions/home.html', context)
 
 def details(request, statename):
-  # We replace the dash with a space for later ease of use. The dash is there because of the slugify filter.
+  attractions = Attraction.objects.all()
   context = {"attractions" : attractions, "statename" : statename.replace("-", " ")}
   return render(request, 'tourist_attractions/details.html', context)
+
+class StateCreate(CreateView):
+  model = State
+  form_class = StateCreateForm
+  template_name = 'tourist_attractions/state_create_form.html'
+  
+class AttractionCreate(CreateView):
+  model = Attraction
+  form_class = AttractionCreateForm
+  template_name = 'tourist_attractions/attraction_create_form.html'
